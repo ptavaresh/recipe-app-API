0a9219d79c053daa2a4c61393ec5fa408508270f
Pedro <ptavaresh@gmail.com>
2019-05-24 11:31:50 -0500

    Added tag serializer, views & url with unittesting for listing and creating objects

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 from core import models
+
 
 def sample_user(email='test@londonappdev.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
 
