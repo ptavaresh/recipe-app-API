ceef6f637556a2182dbe53e59f0869dbca31f2fa
Pedro Tavares <ptavaresh@gmail.com>
2019-05-28 12:30:18 -0500

    Adding file field in Recipe model also the unittest for the model

--- app/app/settings.py
+++ app/app/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 #         'HOST': 'localhost',
 #         'NAME': 'app',
 #         'USER': 'postgres',
-#         'PASSWORD': 'root',
+#         'PASSWORD': 'Ventrue01',
 #     }
 # }
 
@@ -135,7 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 #
 AUTH_USER_MODEL = 'core.User'
